acfbc696ed561da543cf3415fb2cd566566d9f7b
Abhinav4373773 <120887314+Abhinav4373773@users.noreply.github.com>
2024-05-23 14:48:39 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Define the path to your notebook
-notebook_path = "path/to/your_notebook.ipynb"
+notebook_path = "starting_bert.ipynb"
 
 # Execute the notebook
 subprocess.run(["jupyter", "nbconvert", "--to", "notebook", "--execute", "--inplace", notebook_path], check=True)
